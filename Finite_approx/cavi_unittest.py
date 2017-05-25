93eaf842c1154d62705584ea764b48605e13d262
Runjing <runjing_liu@berkeley.edu>
2017-05-25 11:53:31 -0700

    fixed unittest-- changed parameter input to nu_updates

--- Finite_approx/cavi_unittest.py
+++ Finite_approx/cavi_unittest.py
@@ -7,6 +7,8 @@ import autograd.numpy as np
 import autograd.scipy as sp
 from copy import deepcopy
 
+from scipy.stats import bernoulli
+
 def exp_log_likelihood(nu_moment, phi_moment1, phi_moment2, \
                        E_log_pi1, E_log_pi2, Data_shape, sigmas, X, alpha):
 
@@ -39,7 +41,7 @@ def exp_log_likelihood(nu_moment, phi_moment1, phi_moment2, \
     return(y)
 
 # Draw Data
-Num_samples = 10 # sample size
+Num_samples = 5 # sample size
 D = 2 # dimension
 # so X will be a n\times D matrix
 
@@ -90,6 +92,7 @@ class TestCaviUpdates(unittest.TestCase):
     def test_nu_updates(self):
         # initialization for cavi updates
         tau = np.random.uniform(10,100,[K_approx,2])
+        digamma_tau = sp.special.digamma(tau)
         nu = np.random.uniform(0,1,[Num_samples,K_approx])
 
         phi_mu = np.random.normal(0,1,[D,K_approx])
@@ -113,11 +116,12 @@ class TestCaviUpdates(unittest.TestCase):
                                E_log_pi1, E_log_pi2, Data_shape, sigmas, X, alpha)
                 nu_AG = 1/(1 + np.exp(-script_V_AG))
 
-                nu_updates(tau, nu, phi_mu, phi_var, X, sigmas, n, k)
+                nu_updates(tau, nu, phi_mu, phi_var, X, sigmas, n, k, digamma_tau)
+
 
                 # print(np.abs(nu[n,k] - nu_AG[n,k]))
                 self.assertAlmostEqual(nu[n,k] , nu_AG[n,k])
-                
+
     def test_tau_updates(self):
         # initialization for cavi updates
         tau = np.random.uniform(10,100,[K_approx,2])
@@ -188,6 +192,7 @@ class TestCaviUpdates(unittest.TestCase):
             phi_var_AG = -1/(2.*phi2_AG)
             phi_mu_AG = np.dot(phi1_AG, np.diag(phi_var_AG))
 
+
             phi_updates(nu, phi_mu, phi_var, X, sigmas, k) # cavi updates
 
 
@@ -199,6 +204,36 @@ class TestCaviUpdates(unittest.TestCase):
 
             self.assertTrue(np.allclose(phi_mu_AG[:,k], phi_mu[:,k]))
             self.assertTrue(np.allclose(phi_var_AG[k], phi_var[k]))
+
+class TestElboComputation(unittest.TestCase):
+    # initialize variational parameters
+    tau = np.random.uniform(10,100,[K_approx,2])
+    nu = np.random.uniform(0,1,[Num_samples,K_approx])
+
+    phi_mu = np.random.normal(0,1,[D,K_approx])
+    phi_var = np.ones(K_approx)
+
+    z_test = np.zeros((Num_samples, K_approx, 10**4))
+    for n in range(Num_samples):
+        for k in range(K_approx):
+            z_test[n,k,:] = bernoulli.rvs(nu[n,k], 10**4)
+
+    A_test = np.zeros((K_approx, 2,10**4))
+    for k in range(K_approx):
+        for d in range(D):
+            A_test[k,d,:] = np.random.normal(phi_mu[d,k], sigma_A, 10**4)
+
+    Pi = 0
+
+    print(np.shape(A_test))
+    print(np.shape(z_test))
+
+    # compute elbo
+    [elbo,elbo_Term1,elbo_Term2,elbo_Term3,elbo_Term4,elbo_Term5,elbo_Term6,\
+         elbo_Term7] = compute_elbo(tau, nu, phi_mu, phi_var, X, sigmas, alpha)
+
+
+
 
 
 if __name__ == '__main__':
