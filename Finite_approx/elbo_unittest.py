7c4531c7ed747bba61438ba727012db83e499069
Ryan Giordano <rgiordan@gmail.com>
2017-05-31 18:29:40 -0700

    Entropy

--- Finite_approx/elbo_unittest.py
+++ Finite_approx/elbo_unittest.py
@@ -30,8 +30,7 @@ tau, nu, phi_mu, phi_var = \
 
 # compute elbo
 sigmas = {'eps': sigma_eps, 'A': sigma_A}
-elbo, elbo_term1, elbo_term2, elbo_term3, elbo_term4, elbo_term5, elbo_term6, elbo_term7 = \
-    finite_lib.compute_elbo(tau, nu, phi_mu, phi_var, X, sigmas, alpha)
+elbo = finite_lib.compute_elbo(tau, nu, phi_mu, phi_var, X, sigmas, alpha)
 
 # generate samples to compute means
 n_test_samples = 10**6
@@ -39,41 +38,46 @@ n_test_samples = 10**6
 z_sample = np.zeros((Num_samples, K_approx, n_test_samples))
 for n in range(Num_samples):
     for k in range(K_approx):
-        z_sample[n,k,:] = np.random.binomial(1, nu[n,k], size = n_test_samples)
+        z_sample[n,k,:] = np.random.binomial(1, nu[n, k], size=n_test_samples)
 
-A_sample = np.zeros((K_approx, D,n_test_samples))
+A_sample = np.zeros((K_approx, D, n_test_samples))
 for k in range(K_approx):
     for d in range(D):
-        A_sample[k,d,:] = np.random.normal(phi_mu[d,k], phi_var[k], n_test_samples)
+        A_sample[k,d,:] = np.random.normal(phi_mu[d, k], phi_var[k], n_test_samples)
 
 Pi_sample = np.zeros((K_approx, n_test_samples))
 
 for k in range(K_approx):
-    Pi_sample[k,:] = np.random.beta(tau[k,0], tau[k,1], n_test_samples)
+    Pi_sample[k, :] = np.random.beta(tau[k,0], tau[k,1], n_test_samples)
+
 
 class TestElboComputation(unittest.TestCase):
-    def test_term1(self):
-        term1_sample = (alpha/K_approx - 1) * np.sum(np.mean(np.log(Pi_sample), 1))
-        self.assertAlmostEqual(term1_sample, elbo_term1, places = 1)
+    def test_old_elbo(self):
+        print(finite_lib.compute_elbo_old(tau, nu, phi_mu, phi_var, X, sigmas, alpha)[0])
+        print(finite_lib.compute_elbo(tau, nu, phi_mu, phi_var, X, sigmas, alpha))
 
-    def test_term3(self):
-        term3_sample = -np.trace(np.einsum('jkl,jil->ki', A_sample, A_sample)\
-                        /n_test_samples)
-        self.assertAlmostEqual(elbo_term3, \
-            -K_approx*D/2.*np.log(2.*np.pi*sigma_A) + term3_sample/(2*sigma_A), \
-            places = 1)
-            
-    def test_term4(self):
-        term4_sample = 0
-        for n in range(Num_samples):
-            tmp = X[n,:][:,np.newaxis] \
-                    - np.einsum('il,ikl->kl', z_sample[n,:,:], A_sample)
-
-            term4_sample += np.einsum('jk,jk', tmp, tmp)/n_test_samples
-
-        self.assertAlmostEqual(elbo_term4, \
-            -Num_samples*D/2.*np.log(2.*np.pi*sigma_eps) - term4_sample/(2.*sigma_eps), \
-            places = 1)
+    # def test_term1(self):
+    #     term1_sample = (alpha/K_approx - 1) * np.sum(np.mean(np.log(Pi_sample), 1))
+    #     self.assertAlmostEqual(term1_sample, elbo_term1, places = 1)
+    #
+    # def test_term3(self):
+    #     term3_sample = -np.trace(np.einsum('jkl,jil->ki', A_sample, A_sample)\
+    #                     /n_test_samples)
+    #     self.assertAlmostEqual(elbo_term3, \
+    #         -K_approx*D/2.*np.log(2.*np.pi*sigma_A) + term3_sample/(2*sigma_A), \
+    #         places = 1)
+    #
+    # def test_term4(self):
+    #     term4_sample = 0
+    #     for n in range(Num_samples):
+    #         tmp = X[n,:][:,np.newaxis] \
+    #                 - np.einsum('il,ikl->kl', z_sample[n,:,:], A_sample)
+    #
+    #         term4_sample += np.einsum('jk,jk', tmp, tmp)/n_test_samples
+    #
+    #     self.assertAlmostEqual(elbo_term4, \
+    #         -Num_samples*D/2.*np.log(2.*np.pi*sigma_eps) - term4_sample/(2.*sigma_eps), \
+    #         places = 1)
 
 
 if __name__ == '__main__':
--- Finite_approx/valez_finite_VI_lib.py
+++ Finite_approx/valez_finite_VI_lib.py
@@ -12,7 +12,7 @@ from copy import deepcopy
 import math
 
 
-# Data_shape: D,N,K
+# data_shape: D,N,K
 
 def phi_updates(nu, phi_mu, phi_var, X, sigmas, k):
 
@@ -45,7 +45,8 @@ def nu_updates(tau, nu, phi_mu, phi_var, X, sigmas, n, k, digamma_tau):
 
     nu_term2 = (1. / (2. * s_eps)) * (phi_var[k]*D + np.dot(phi_mu[:,k], phi_mu[:,k]))
 
-    nu_term3 = (1./s_eps) * np.dot(phi_mu[:, k], X[n, :] - np.dot(phi_mu, nu[n, :]) + nu[n,k] * phi_mu[:, k])
+    nu_term3 = (1./s_eps) * np.dot(phi_mu[:, k], X[n, :] - \
+               np.dot(phi_mu, nu[n, :]) + nu[n,k] * phi_mu[:, k])
 
     script_V = nu_term1 - nu_term2 + nu_term3
 
@@ -82,10 +83,10 @@ def cavi_updates(tau, nu, phi_mu, phi_var, X, alpha, sigmas):
     tau_updates(tau, nu, alpha)
 
 
-def compute_elbo(tau, nu, phi_mu, phi_var, X, sigmas, alpha):
+def compute_elbo_old(tau, nu, phi_mu, phi_var, X, sigmas, alpha):
 
     sigma_eps = sigmas['eps']
-    sigma_A = sigmas['A']
+    sigma_a = sigmas['A']
     D = np.shape(X)[1]
     N = np.shape(X)[0]
     K = np.shape(phi_mu)[1]
@@ -100,16 +101,18 @@ def compute_elbo(tau, nu, phi_mu, phi_var, X, sigmas, alpha):
                 - N * np.sum(digamma_sum_tau)
 
     elbo_term3 = \
-        -K*D/2.*np.log(2.*np.pi*sigma_A) - 1./(2.*sigma_A) *\
+        -K*D/2.*np.log(2.*np.pi*sigma_a) - 1./(2.*sigma_a) *\
         (np.sum(phi_var)*D + np.trace(np.dot(phi_mu.T , phi_mu)))
 
     elbo_term4 = \
         -N * D/2. * np.log(2. * np.pi * sigma_eps)\
         -1/(2. * sigma_eps) * np.trace(np.dot(X.T, X)) \
         +1/(sigma_eps) * np.trace(np.dot(np.dot(nu, phi_mu.T), X.T))\
-        -1/(2. * sigma_eps) * np.sum(np.dot(nu, D*phi_var + np.diag(np.dot(phi_mu.T, phi_mu)))) \
-        -1/(2.*sigma_eps) * np.trace(np.dot(np.dot(nu, np.dot(phi_mu.T, phi_mu) - np.identity(K) \
-                                                    * np.diag(np.dot(phi_mu.T, phi_mu))), nu.T))\
+        -1/(2. * sigma_eps) * np.sum(np.dot(nu, D*phi_var + \
+            np.diag(np.dot(phi_mu.T, phi_mu)))) \
+        -1/(2.*sigma_eps) * np.trace(np.dot(np.dot( \
+            nu, np.dot(phi_mu.T, phi_mu) - np.identity(K) \
+            * np.diag(np.dot(phi_mu.T, phi_mu))), nu.T))
 
     # The log beta function is not in autograd's scipy.
     log_beta = sp.special.gammaln(tau[:,0]) + sp.special.gammaln(tau[:,1]) \
@@ -125,26 +128,93 @@ def compute_elbo(tau, nu, phi_mu, phi_var, X, sigmas, alpha):
 
     elbo_term7 = np.sum(np.sum( -np.log(nu ** nu) - np.log((1.-nu) ** (1.-nu)) ))
 
-    elbo = elbo_term1 + elbo_term2 + elbo_term3 + elbo_term4 + elbo_term5 + elbo_term6 + elbo_term7
+    elbo = elbo_term1 + elbo_term2 + elbo_term3 + elbo_term4 + elbo_term5 + \
+           elbo_term6 + elbo_term7
 
-    return(elbo, elbo_term1, elbo_term2, elbo_term3, elbo_term4, elbo_term5, elbo_term6, elbo_term7)
+    return(elbo, elbo_term1, elbo_term2, elbo_term3, elbo_term4, \
+          elbo_term5, elbo_term6, elbo_term7)
+
+
+def nu_entropy(nu):
+    log_1mnu = np.log(1 - nu)
+    log_nu = np.log(nu)
+    return -1 * np.sum(nu * (log_nu - log_1mnu) + log_1mnu)
+
+
+def phi_entropy(phi_var, D):
+    return 0.5 * D * np.sum(np.log(2. * np.pi * phi_var))
+
+
+def pi_entropy(tau):
+    digamma_tau0 = sp.special.digamma(tau[:, 0])
+    digamma_tau1 = sp.special.digamma(tau[:, 1])
+    digamma_tausum = sp.special.digamma(np.sum(tau, 1))
+
+    lgamma_tau0 = sp.special.gammaln(tau[:, 0])
+    lgamma_tau1 = sp.special.gammaln(tau[:, 1])
+    lgamma_tausum = sp.special.gammaln(np.sum(tau, 1))
+
+    lbeta = lgamma_tau0 + lgamma_tau1 - lgamma_tausum
+
+    return np.sum(
+        lbeta - \
+        (tau[:, 0] - 1.) * digamma_tau0 - \
+        (tau[:, 1] - 1.) * digamma_tau1 + \
+        (tau[:, 0] + tau[:, 1] - 2) * digamma_tausum)
+
+
+def get_moments(tau, nu, phi_mu, phi_var):
+    lgamma_tausum = sp.special.gammaln(np.sum(tau, 1))
+    e_log_pi1 = sp.special.gammaln(tau[:, 0]) - lgamma_tausum
+    e_log_pi2 = sp.special.gammaln(tau[:, 1]) - lgamma_tausum
+
+    nu_moment = nu
+
+    D = phi_mu.shape[0]
+    phi_moment1 = phi_mu
+    phi_moment2 = np.diag(np.dot(phi_mu.T, phi_mu) + D * phi_var)
+
+    return e_log_pi1, e_log_pi2, phi_moment1, phi_moment2, nu_moment
+
+
+def compute_elbo(tau, nu, phi_mu, phi_var, X, sigmas, alpha):
+    # Check for appropriate shapes.
+    assert X.shape[1] == phi_mu.shape[0]
+    assert nu.shape[0] == X.shape[0]
+    assert nu.shape[1] == phi_mu.shape[1]
+    assert len(phi_var) == phi_mu.shape[1]
+    assert tau.shape[0] == nu.shape[1]
+    assert tau.shape[1] == 2
+
+    e_log_pi1, e_log_pi2, phi_moment1, phi_moment2, nu_moment = \
+        get_moments(tau, nu, phi_mu, phi_var)
+
+    e_log_lik = exp_log_likelihood(
+        nu_moment, phi_moment1, phi_moment2,  e_log_pi1, e_log_pi2, \
+        sigmas, X, alpha)
+
+    D = X.shape[1]
+    entropy = nu_entropy(nu) + phi_entropy(phi_var, D) + pi_entropy(tau)
+
+    return e_log_lik + entropy
 
 
 def exp_log_likelihood(nu_moment, phi_moment1, phi_moment2, \
-                       E_log_pi1, E_log_pi2, Data_shape, sigmas, X, alpha):
+                       e_log_pi1, e_log_pi2, sigmas, X, alpha):
 
     sigma_eps = sigmas['eps']
-    sigma_A = sigmas['A']
-    D = Data_shape['D']
-    N = Data_shape['N']
-    K = Data_shape['K']
+    sigma_a = sigmas['A']
+
+    D = X.shape[1]
+    N = X.shape[0]
+    K = nu_moment.shape[1]
 
     # Compute the beta, bernoulli, and A terms.
-    beta_lh = (alpha / K - 1.) * np.sum(E_log_pi1)
-    bern_lh = np.sum(nu_moment * (E_log_pi1 - E_log_pi2)) + N * np.sum(E_log_pi2)
-    norm_a_term = -0.5 * np.sum(phi_moment2) / sigma_A
+    beta_lh = (alpha / K - 1.) * np.sum(e_log_pi1)
+    bern_lh = np.sum(nu_moment * (e_log_pi1 - e_log_pi2)) + N * np.sum(e_log_pi2)
+    norm_a_term = -0.5 * np.sum(phi_moment2) / sigma_a
 
-    ## compute the data likelihood term
+    # Compute the data likelihood term
     phi_moment1_outer = np.matmul(phi_moment1.T, phi_moment1)
     phi_moment1_outer = phi_moment1_outer - np.diag(np.diag(phi_moment1_outer))
     norm_x_nu_quadratic = \
@@ -156,52 +226,22 @@ def exp_log_likelihood(nu_moment, phi_moment1, phi_moment2, \
     return beta_lh + bern_lh + norm_a_term + norm_x_term
 
 
-# def exp_log_likelihood_old(nu_moment, phi_moment1, phi_moment2, \
-#                            E_log_pi1, E_log_pi2, Data_shape, sigmas, X, alpha):
-#
-#     sigma_eps = sigmas['eps']
-#     sigma_A = sigmas['A']
-#     D = Data_shape['D']
-#     N = Data_shape['N']
-#     K = Data_shape['K']
-#
-#     beta_lh = (alpha/K - 1.)*np.sum(E_log_pi1)
-#     bern_lh = np.sum(np.dot(nu_moment[n,:], E_log_pi1) \
-#                             + np.dot(1.-nu_moment[n,:], E_log_pi2) for n in range(N))
-#     Normal_A = -1/(2.*sigma_A) * np.sum(phi_moment2)
-#
-#     Normal_X_sum = 0
-#     ## compute the data likelihood term
-#     for n in range(N):
-#         dum1 = 2.*np.sum(np.sum(nu_moment[n,i] * nu_moment[n,j] *
-#                                 np.dot(phi_moment1[:,i], phi_moment1[:,j]) \
-#                                 for i in range(j)) for j in range(K))
-#         dum2 = np.dot(nu_moment[n,:] , phi_moment2 )
-#
-#         dum3 = -2. * np.dot(X[n,:], np.dot(phi_moment1, nu_moment[n,:]))
-#
-#         # dum4 = np.dot(X[n,:], X[n,:])
-#         Normal_X_sum += dum1 + dum2 + dum3
-#
-#     Normal_X = -1/(2*sigma_eps)*Normal_X_sum
-#
-#     y = beta_lh + bern_lh + Normal_A + Normal_X
-#     return(y)
-
-
 def initialize_parameters(Num_samples, D, K_approx):
-    tau = np.random.uniform(0, 1, [K_approx, 2]) # tau1, tau2 -- beta parameters for v
-    nu =  np.random.uniform(0, 1, [Num_samples, K_approx]) # Bernoulli parameter for z_nk
+    # tau1, tau2 -- beta parameters for v
+    tau = np.random.uniform(0, 1, [K_approx, 2])
 
-    phi_mu = np.random.normal(0, 1, [D, K_approx]) # kth mean (D dim vector) in kth column
+    # Bernoulli parameter for z_nk
+    nu =  np.random.uniform(0, 1, [Num_samples, K_approx])
+
+    # kth mean (D dim vector) in kth column
+    phi_mu = np.random.normal(0, 1, [D, K_approx])
     phi_var = np.ones(K_approx)
 
     return tau, nu, phi_mu, phi_var
 
 
-def generate_data(Num_samples, D, K_inf, sigma_A, sigma_eps, alpha):
+def generate_data(Num_samples, D, K_inf, sigma_a, sigma_eps, alpha):
     Pi = np.ones(K_inf) * .8
-    # Pi = np.random.beta(alpha/K_inf, 1)
 
     Z = np.zeros([Num_samples, K_inf])
 
@@ -212,15 +252,12 @@ def generate_data(Num_samples, D, K_inf, sigma_A, sigma_eps, alpha):
     Z = np.random.binomial(1, Pi, [ Num_samples, K_inf ])
 
     # Draw A from multivariate normal
-    # A = np.random.multivariate_normal(mu, sigma_A * np.identity(D), K_inf)
-    A = np.random.normal(0, np.sqrt(sigma_A), (K_inf, D))
+    A = np.random.normal(0, np.sqrt(sigma_a), (K_inf, D))
 
     # draw noise
-    #epsilon = np.random.multivariate_normal(
-    #    np.zeros(D), sigma_eps*np.identity(D), Num_samples)
     epsilon = np.random.normal(0, np.sqrt(sigma_eps), (Num_samples, D))
 
     # the observed data
-    X = np.dot(Z,A) + epsilon
+    X = np.matmul(Z, A) + epsilon
 
     return Pi, Z, mu, A, X
