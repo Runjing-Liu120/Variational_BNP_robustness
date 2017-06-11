7220fd760c7759d714d757ff4048928be62cb1a5
Runjing <runjing_liu@berkeley.edu>
2017-06-10 17:53:23 -0700

    unittest for rank-1-update

--- finite_approx/unit_tests/gibbs_sampler_lib_unittest.py
+++ finite_approx/unit_tests/gibbs_sampler_lib_unittest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import unittest
-from finite_approx.gibbs_sampler_lib import GibbsSampler
+from finite_approx.gibbs_sampler_lib import GibbsSampler, update_inv_var
 import finite_approx.valez_finite_VI_lib as vi
 from copy import deepcopy
 import numpy as np
@@ -36,6 +36,30 @@ class TestGibbsSampler(unittest.TestCase):
         self.assertEqual(len(gibbs_sampler.a_draws), n_draws)
         self.assertEqual(len(gibbs_sampler.pi_draws), n_draws)
         self.assertEqual(len(gibbs_sampler.z_draws), n_draws)
+
+class TestCollapsedGIbbsSampler(unittest.TestCase):
+    def test_rank_1_update(self):
+        sigma_eps = 0.2
+        sigma_A = 0.3
+        Z = np.random.binomial(1, 0.5, (5,3))
+        K = np.shape(Z)[1]
+        N = np.shape(Z)[0]
+
+        for n in range(N):
+            for k in range(K):
+                Z_flip = deepcopy(Z)
+                Z_flip[n,k] = 1 - Z[n,k]
+
+                var_inv = np.linalg.inv(np.dot(Z.T, Z) \
+                        + sigma_eps/sigma_A * np.eye(K))
+                var_flip = np.dot(Z_flip.T, Z_flip) \
+                        + sigma_eps/sigma_A * np.eye(K)
+
+                inv_var_flip \
+                    = update_inv_var(Z, var_inv, sigma_eps, sigma_A, n, k)
+
+                self.assertTrue(np.allclose(inv_var_flip, np.linalg.inv(var_flip)))
+
 
 
 if __name__ == '__main__':
